c6fbaa2c88cd5a50551cc2a5a2d3adc8361d55d1
acatree <academictree.kr@gmail.com>
2025-10-05 19:11:33 +0900

    Update main.py

--- article_kor/main.py
+++ article_kor/main.py
@@ -166,4 +166,4 @@ def generate_paper(topic, api_key=None):
                 asset_files.append(os.path.join(folder, file))
 
     # 10. 반환
-    return tex_file, "references.bib", asset_files
+    return tex_file, "references.bib", asset_files, creative_title, research_topic
